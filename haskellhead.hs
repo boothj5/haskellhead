e6a0db7b75a5175c9b064d1c7c722cf9ae646bf1
James Booth <jamesbooth2000@hotmail.com>
2011-01-02 18:14:09 +0000

    Using showRank function

--- haskellhead.hs
+++ haskellhead.hs
@@ -1,4 +1,19 @@
+ranks :: [[Char]]
 ranks = ["TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING", "ACE"]
+
+ranksNum :: [Integer]
+ranksNum = [2,3..14]
+
+suits :: [[Char]]
 suits = ["HEARTS", "CLUBS", "DIAMONDS", "SPADES"]
 
-deck = [rank ++ " of " ++ suit | suit <- suits, rank <- ranks]
+showRank :: (Num a, Ord a) => a -> String
+showRank a | a >= 2 && a <=10 = show a
+showRank 11                   = "JACK"
+showRank 12                   = "QUEEN"
+showRank 13                   = "KING"
+showRank 14                   = "ACE"
+showRank _                    = error "Not a card"
+
+deck :: [[Char]]
+deck = [showRank rank ++ " of " ++ suit | suit <- suits, rank <- ranksNum]
