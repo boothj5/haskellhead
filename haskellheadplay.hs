cb82024fc6dbd0faee024dcc19726bd37b4f584f
James Booth <jamesbooth2000@hotmail.com>
2011-01-08 18:28:56 +0000

    Renamed mucking about haskellhead

--- haskellheadplay.hs
+++ haskellheadplay.hs
@@ -2,15 +2,16 @@
 -- Card data types
 -- ---------------
 
--- Using enum for rank
-data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace 
             deriving (Show, Eq, Ord, Enum)
 
 data Suit = Hearts | Clubs | Diamonds | Spades deriving (Show, Eq)
 
-data Card = Card Rank Suit deriving (Show, Eq)
+data Card = Card Rank Suit deriving (Eq)
+
+instance Show Card where
+    show (Card rank suit) = show rank ++ " of " ++ show suit
 
 deck :: [Card]
 deck = [Card rank suit | suit <- [Hearts, Clubs, Diamonds, Spades], rank <- [Two .. Ace]]
 
-showCard (Card rank suit) = show rank ++ " of " ++ show suit
