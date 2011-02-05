905926db2e279634b2b71bb7d746083bb185c28d
James Booth <jamesbooth2000@hotmail.com>
2011-02-05 17:01:33 +0000

    Created newtype for list and implemented monoid

--- linkedlist.hs
+++ linkedlist.hs
@@ -1,17 +1,29 @@
 import Control.Applicative
+import Data.Monoid
   
 data LinkedList a = EmptyList | Node a (LinkedList a)
     deriving (Show)
 
+newtype ConcList a = ConcList { getConcList :: LinkedList a }
+    deriving (Show)
+
 instance Functor LinkedList where
-    fmap f EmptyList = EmptyList
+    fmap f EmptyList          = EmptyList
     fmap f (Node item (rest)) = Node (f item) (fmap f (rest))
 
 instance Applicative LinkedList where
-   pure n = Node n (pure n)
-   EmptyList <*> _ = EmptyList
-   _ <*> EmptyList = EmptyList
-   (Node f (r1)) <*> (Node n (r2)) = Node (f n) (r1 <*> r2)
+    pure n = Node n (pure n)
+    EmptyList <*> _ = EmptyList
+    _ <*> EmptyList = EmptyList
+    (Node f (r1)) <*> (Node n (r2)) = Node (f n) (r1 <*> r2)
+--    (Node n (r1)) <*> (Node f (r2)) = Node (f n) (r1 <*> r2)
+
+instance Monoid (ConcList a) where
+    mempty = ConcList EmptyList
+    mappend l (ConcList EmptyList) = l
+    mappend (ConcList EmptyList) l = l
+    mappend (ConcList (Node n (EmptyList))) l2 = ConcList (Node n (getConcList l2))
+    mappend (ConcList (Node n r1)) l2 = ConcList (Node n (getConcList (((ConcList r1) `mappend` l2))))
 
 listInsert :: (Ord a) => a -> LinkedList a -> LinkedList a
 listInsert item EmptyList       = Node item (EmptyList)
@@ -31,7 +43,7 @@ ifDivBy3Add2 n | (rem n 3 /= 0) = n
 
 main = do
     let myList = Node 1 (Node 2 (Node 3 (Node 4 (Node 5 (Node 6 (Node 7 (Node 8 (Node 9 (Node 10 EmptyList)))))))))
-    putStrLn "myList:"
+    putStrLn "myList"
     putStrLn $ show myList
     putStrLn ""
     putStrLn "Elements 0, 5 and 10"
@@ -41,43 +53,57 @@ main = do
     putStrLn ""
  
     let newList = fmap (\value -> value * 100) myList
-    putStrLn "Multiply each item by 100 using fmap"
+    putStrLn "newList = fmap (\\value -> value * 100) myList"
     putStrLn $ show newList
     putStrLn ""
 
     let biggerThan2 = fmap (>2) myList
-    putStrLn "Test each bigger than 2 using fmap"
+    putStrLn "biggerThan2 = fmap (>2) myList"
     putStrLn $ show biggerThan2
     putStrLn ""
  
     let anotherList = fmap ifDivBy3Add2 myList
-    putStrLn "If divisible by 3 add 2"
+    putStrLn "anotherList = fmap ifDivBy3Add2 myList"
     putStrLn $ show anotherList
     putStrLn ""
  
     let functionList = fmap (*) myList
         mappedFunctionList = fmap (\value -> value 2) functionList
-    putStrLn "fmap a function (which takes a function and passes it 2) to a list"
-    putStrLn "containing items of the function (item *)"     
+    putStrLn "functionList = fmap (*) myList"
+    putStrLn "mappedFunctionList = fmap (\\value -> value 2) functionList"
     putStrLn $ show mappedFunctionList
     putStrLn ""
  
     let applicativeMappedList = functionList <*> myList
-    putStrLn "Apply the list with item = (item *) to the myList, essentially squaring the list"
+    putStrLn "applicativeMappedList = functionList <*> myList"
     putStrLn $ show applicativeMappedList
     putStrLn ""
  
     let addedLists = pure (+) <*> myList <*> applicativeMappedList
-    putStrLn "Add myList to the previous list"
+    putStrLn "addedLists = pure (+) <*> myList <*> applicativeMappedList"
     putStrLn $ show addedLists
     putStrLn ""
  
     let alternativeAddedLists = (+) <$> myList <*> applicativeMappedList
-    putStrLn "Add with '(+) <$> list1 <*> list2' instead of 'pure (+) <*> list1 <*> list2'"
+    putStrLn "alternativeAddedLists = (+) <$> myList <*> applicativeMappedList"
     putStrLn $ show alternativeAddedLists
     putStrLn ""
  
     let threeAddedLists = (+) <$> ((+) <$> myList <*> applicativeMappedList) <*> newList
-    putStrLn "Add the *100 list too with  '(+) <$> ((+) <$> list1 <*> list2) <*> list3'"
+    putStrLn "threeAddedLists = (+) <$> ((+) <$> myList <*> applicativeMappedList) <*> newList"
     putStrLn $ show threeAddedLists
+    putStrLn ""
+    
+    let orderList = Node "First:" (Node "Second:" (Node "Third:" (Node "Fourth:" EmptyList)))
+        someStrList = Node "James" (Node "Steve" (Node "Dave" (Node "Mike" EmptyList)))
+        concatList = (++) <$> orderList <*> someStrList
+    putStrLn "orderList:"
+    putStrLn $ show orderList
+    putStrLn "someStrList:"
+    putStrLn $ show someStrList
+    putStrLn "concatList = (++) <$> orderList <*> someStrList"
+    putStrLn $ show concatList
+    putStrLn ""
+ 
+
 
