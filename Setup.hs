b5196e8b87275736613e46f28cdbcba8ebe10daf
James Booth <jamesbooth2000@hotmail.com>
2011-03-10 11:02:53 +0000

    Started using traditional Setup.hs method for build

---
+++ Setup.hs
@@ -0,0 +1,18 @@
+#! /usr/bin/env runhaskell
+
+import Distribution.Simple
+import Distribution.PackageDescription(PackageDescription)
+import Distribution.Simple.LocalBuildInfo(LocalBuildInfo)
+import System.Cmd(system)
+import Distribution.Simple.LocalBuildInfo
+
+main = defaultMainWithHooks (simpleUserHooks {runTests = runAllTests, postHaddock = runHlint})
+
+runAllTests a b pd lb = do
+    system ( "runhaskell -i./src ./testsuite/tests/CardTests.hs") >> return()
+    system ( "runhaskell -i./src ./testsuite/tests/PlayerTests.hs") >> return()
+    system ( "runhaskell -i./src ./testsuite/tests/GameTests.hs") >> return()
+
+runHlint a b pd lb = do
+    system ("hlint ./src/*.hs --report=./dist/doc/html/haskellhead/haskellhead-hlint.html") >> return ()
+    system ("hlint ./testsuite/tests/*.hs --report=./dist/doc/html/haskellhead/tests-hlint.html") >> return ()
--- src/Card.hs
+++ src/Card.hs
@@ -1,3 +1,4 @@
+-- | Module for represneting cards and the functions that may be performed on them
 module Card 
 ( Card(Card)
 , Rank(Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace)
@@ -30,28 +31,32 @@ data Card = Card { rank :: Rank, suit :: Suit }
 instance Show Card where
     show (Card rank suit) = fmap toUpper (show rank) ++ " of " ++ fmap toUpper (show suit)
 
-------------------------------------------------
---
--- Card functions
---
+-- | Card which will burn the pile
 burnRank :: Rank
 burnRank = Ten
 
+-- | Card which skip the next player
 missAGoRank :: Rank
 missAGoRank = Eight
 
+-- | When layed, doesn't have effect on last card
 invisibleRank :: Rank
 invisibleRank = Seven
 
+-- | Brings pile back to this rank
 resetRank :: Rank
 resetRank = Two
 
+-- | test whether a card may be layed on anything
 layOnAnythingCard :: Card -> Bool
 layOnAnythingCard c = rank c `elem` layOnAnyThingRanks
 
+-- | test whether to cards have the same rank
 ranksAreEqual :: Card -> Card -> Bool
 ranksAreEqual c1 c2 = rank c1 == rank c2
 
+-- | Compare two cards, special cards are considered higher than any other
+-- but are considered equal to each other
 compareCardsSpecialHighest :: Card -> Card -> Ordering
 compareCardsSpecialHighest c1 c2 
     | layOnAnythingCard c1 && layOnAnythingCard c2 = EQ
@@ -59,9 +64,11 @@ compareCardsSpecialHighest c1 c2
     | not (layOnAnythingCard c1) && layOnAnythingCard c2 = LT
     | otherwise = compare c1 c2
     
+-- | Cards that can be layed on anything
 layOnAnyThingRanks :: [Rank]
 layOnAnyThingRanks = [burnRank, invisibleRank, resetRank]
 
+-- | Test whether all cards in the list have the same rank
 allRanksSame :: [Card] -> Bool
 allRanksSame [] = False
 allRanksSame (_:[]) = True
